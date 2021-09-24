31b7ea33ad4a579e80e4d342728159d355b19778
Rosty Semenyshyn <ripka.ua@gmail.com>
2021-09-24 16:16:31 +0200

    fixed mapping

--- src/_helpers/api_mapping.js
+++ src/_helpers/api_mapping.js
@@ -2,7 +2,8 @@ import { routes } from './constants';
 
 export function notionPageToObj(p) {
 	const url = routes.PAGE.replace(':id', p.id);
-	const img = p.cover && p.cover.external && p.cover.external.url ? p.cover.external.url : '';
+	const imgType = p.cover && p.cover.type ? p.cover.type : '';
+ 	const img = imgType && p.cover['imgType'] && p.cover['imgType'].url ? p.cover['imgType'].url : '';
 	const title = p.properties['Name'].title.length ? p.properties['Name'].title[0].plain_text : '';
 	const tags = p.properties.tags ? p.properties.tags.multi_select : [];
 	const date = p.properties['Created'] ? p.properties['Created'].created_time : '';
